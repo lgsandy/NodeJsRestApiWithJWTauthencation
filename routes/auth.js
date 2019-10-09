ec8999dfa54de6b81332f8b2346d34f2919c2ad1
sandy <32234544+lgsandy@users.noreply.github.com>
2019-10-09 11:05:52 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -54,7 +54,7 @@ router.post('/login', (req, res) => {
 			//compare password
 			if (bcrypt.compareSync(req.body.password, user.password)) {
 				//create token and send that token to clint as heder
-				const token = jwt.sign({ _id: user._id }, 'sgdjsghghjghj',{ expiresIn: '1h' });
+				const token = jwt.sign({ _id: user._id }, 'sgdjsghghjghj');
 				//set this token to header
 				res.header('auth-token', token).send(token);
 			} else {
