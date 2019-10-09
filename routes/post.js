7d1d690b5aafde8b86e8ca366d0960a41cf1e6dd
sandy <32234544+lgsandy@users.noreply.github.com>
2019-10-09 11:48:25 +0530

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,20 @@
+const UserModel = require('../model/UserModel');
 const express = require('express');
 const router = express.Router();
 //import verify token for this route
 const verify = require('./verifyToken');
 //create get post route
 router.get('/', verify, (req, res) => {
-	res.json({ posts: { title: 'sandy Post', desc: 'Authencated data' } });
+	UserModel.find()
+		.select('name email')
+		.exec()
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => {
+			res.send(err);
+		});
+	//res.json({ posts: { title: 'sandy Post', desc: 'Authencated data' } });
 });
 //export router
 module.exports = router;
